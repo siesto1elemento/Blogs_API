30e56b31ee486be539c04c62d87ac64da1ff7fb1
siesto1elemento <rohitojha9720@gmail.com>
2023-10-25 00:12:35 +0530

    updated views

--- blogs_api/backend/serializers.py
+++ blogs_api/backend/serializers.py
@@ -2,22 +2,37 @@ from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=200)
-    description = serializers.CharField()
-    slug = serializers.SlugField(max_length=200, )
-    published = serializers.DateTimeField(read_only=True)
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model: Article
+        fields = '__all__'
+
+
+
+
+
+
+
+
+
+
+
+# class ArticleSerializer(serializers.Serializer):
+#     title = serializers.CharField(max_length=200)
+#     description = serializers.CharField()
+#     slug = serializers.SlugField(max_length=200, )
+#     published = serializers.DateTimeField(read_only=True)
     
     
-    def create(self, validated_data):
-        return Article.objects.create(**validated_data)
+#     def create(self, validated_data):
+#         return Article.objects.create(**validated_data)
     
-    def update(self, instance, validated_data):
-        instance.title = validated_data.get('title', instance.title)
-        instance.description = validated_data.get('description', instance.description)
-        instance.slug = validated_data.get('slug', instance.slug)
-        instance.published = validated_data.get('published', instance.published)
-        instance.save()
+#     def update(self, instance, validated_data):
+#         instance.title = validated_data.get('title', instance.title)
+#         instance.description = validated_data.get('description', instance.description)
+#         instance.slug = validated_data.get('slug', instance.slug)
+#         instance.published = validated_data.get('published', instance.published)
+#         instance.save()
         
-        return instance
+#         return instance
         

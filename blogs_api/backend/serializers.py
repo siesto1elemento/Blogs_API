f2ecaa83f172725d484031d543f92c9e00811cd9
siesto1elemento <rohitojha9720@gmail.com>
2023-10-25 14:46:54 +0530

    included api decorators

--- blogs_api/backend/serializers.py
+++ blogs_api/backend/serializers.py
@@ -3,6 +3,7 @@ from .models import Article
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Article
         fields = '__all__'
--- blogs_api/backend/views.py
+++ blogs_api/backend/views.py
@@ -6,19 +6,55 @@ from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+@api_view(['GET','POST'])
 def article_list(request):
-    
-    if request.method == 'GET':
+    if request.method == "GET":
         articles = Article.objects.all()
-        serializer = ArticleSerializer(articles, many=True)
-        
-        return JsonResponse(serializer.data, safe=False)
+        serializer = ArticleSerializer(articles, many = True)
+        return Response(serializer.data)
     
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def article_list(request):
+    
+#     if request.method == 'GET':
+#         articles = Article.objects.all()
+#         serializer = ArticleSerializer(articles, many=True)
+        
+#         return JsonResponse(serializer.data, safe=False)
+    
+#     elif request.method == 'POST':
+#         data = JSONParser().parse(request)
+#         serializer = ArticleSerializer(data=data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return JsonResponse(serializer.data, status=201)
+#         return JsonResponse(serializer.errors, status=400)

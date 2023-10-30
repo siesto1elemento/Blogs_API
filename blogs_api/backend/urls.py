36e867d436de23207c4c04d40600db0414c64fce
siesto1elemento <rohitojha9720@gmail.com>
2023-10-30 11:28:41 +0530

    added detailed article views

--- blogs_api/backend/urls.py
+++ blogs_api/backend/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list,article_details
 
 
 urlpatterns = [
-    path('articles/',article_list)
+    path('articles/',article_list),
+    path('articles/<slug:slug>/',article_details),
+    
 ]
--- blogs_api/backend/views.py
+++ blogs_api/backend/views.py
@@ -28,10 +28,29 @@ def article_list(request):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+@api_view(['GET','DELETE','PUT'])
+def article_details(request, slug):
+    try:
+        article = Article.objects.get(slug=slug)
+    except Article.DoesNotExist():
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
+    
+    if request.method == "GET":
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+    
+    elif request.method == "PUT":
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-
-
+    elif request.method == "DELETE":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
 
 
 

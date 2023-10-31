c6f37a4768a4427882944349f1f7de0647c185d1
siesto1elemento <rohitojha9720@gmail.com>
2023-10-31 18:32:06 +0530

    Added authentication(session and token)

--- blogs_api/backend/urls.py
+++ blogs_api/backend/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import article_list,article_details
+from django.urls import path, include
+#from .views import ArticleList,ArticleDetails
+from rest_framework.routers import DefaultRouter
+from .views import ArticleViewSet
+
+
+router = DefaultRouter()
+router.register('articles', ArticleViewSet, basename='articles')
 
 
 urlpatterns = [
-    path('articles/',article_list),
-    path('articles/<slug:slug>/',article_details),
-    
+    #path('articles/', article_list),
+    #path('articles/<slug:slug>/', article_details),
+    #path('articles/', ArticleList.as_view()),
+    #path('articles/<slug:slug>/', ArticleDetails.as_view())
+    path('', include(router.urls))
 ]
